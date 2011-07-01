e086a1992ea0213045fca58f66bba2a32b578907
Sean Nieuwoudt <sean@isean.co.za>
2011-07-01 12:54:53 +0200

    Adding info link to noise words example

--- examples/noise_words.php
+++ examples/noise_words.php
@@ -3,6 +3,8 @@
 
 require "../gump.class.php";
 
+// What are noise words? http://support.dtsearch.com/webhelp/dtsearch/noise_words.htm
+
 $_POST = array(
 	'words' => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English"
 );
