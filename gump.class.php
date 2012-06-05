c11386a109632a477b96e91660fb5b8d5959074f
Sean Nieuwoudt <sean@isean.co.za>
2012-06-05 19:51:00 +0200

    Missing deleted file

--- gump.class.php
+++ gump.class.php
@@ -21,7 +21,7 @@ class GUMP
 				  				  	 the,their,them,then,there,these,they,this,those,through,to,too,under,up,
 				  				  	 very,was,way,we,well,were,what,where,which,while,who,with,would,you,your,a,
 				  				  	 b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,$,1,2,3,4,5,6,7,8,9,0,_";
-
+				
 	// ** ------------------------- Validation Helpers ---------------------------- ** //	
 	
 	/**
