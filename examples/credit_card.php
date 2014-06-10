41da5cbfd5f856319761719676ea37e53a825cfb
Sean Nieuwoudt <sean@wixelhq.com>
2014-06-10 07:52:47 +0200

    Fix: syntax error in credit_card.php example file

--- examples/credit_card.php
+++ examples/credit_card.php
@@ -6,7 +6,7 @@ require "../gump.class.php";
 $validator = new GUMP();
 
 $_POST = array(
-	'cc' => '987230234-2498234-24234-23' // This is not a valid credit card number
+	'cc' => '987230234-2498234-24234-23'; // This is not a valid credit card number
 )
 
 $rules = array(
