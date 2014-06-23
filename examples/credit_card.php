27b758fb731df8958815d0f543b0141bffaa231d
Ryan H <ry167@ry167.com>
2014-06-23 22:41:46 +1200

    syntax fix
    ; was in the array(), not at the end of it.

--- examples/credit_card.php
+++ examples/credit_card.php
@@ -6,8 +6,8 @@ require "../gump.class.php";
 $validator = new GUMP();
 
 $_POST = array(
-	'cc' => '987230234-2498234-24234-23'; // This is not a valid credit card number
-)
+	'cc' => '987230234-2498234-24234-23' // This is not a valid credit card number
+);
 
 $rules = array(
 	'cc' => 'valid_cc'
