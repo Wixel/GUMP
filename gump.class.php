a8ffc62892ffe080dd66e751f627a2f0d562af81
Robert Witchell <robertwitchell@gmail.com>
2014-11-23 15:00:06 +1100

    aligned $fieldCharsToRemove variable to Gump's default settings for public release.

--- gump.class.php
+++ gump.class.php
@@ -43,7 +43,7 @@ class GUMP
 				  				  	 b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,$,1,2,3,4,5,6,7,8,9,0,_";
 
 	// field characters below will be replaced with a space.
-	protected $fieldCharsToRemove = array('-') ; //array('_','-');
+	protected $fieldCharsToRemove = array('_','-');
 	
 	// ** ------------------------- Validation Helpers ---------------------------- ** //	
 	
