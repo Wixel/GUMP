b8f2a8a26a32a72bf243f2595a86105a4a52a717
Filis Futsarov <filisfutsarov@gmail.com>
2020-02-14 22:01:20 +0100

    Tests

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophet;
+use GUMP;
+use ReflectionClass;
+
+abstract class BaseTestCase extends TestCase
+{
+    protected $prophet;
+
+    /**
+     * @var GUMP
+     */
+    protected $gump;
+
+    public function setUp(): void
+    {
+        $this->prophet = new Prophet;
+        $this->gump = new GUMP;
+    }
+
+    protected function tearDown(): void
+    {
+        $this->prophet->checkPredictions();
+
+        $this->resetCustomFieldsLabels();
+        $this->resetCustomValidators();
+        $this->resetCustomFilters();
+        unset($this->gump);
+    }
+
+    public static function setPrivateField(string $class, $object, $property, $value)
+    {
+        $reflectionClass = new ReflectionClass($class);
+        $reflectionProperty = $reflectionClass->getProperty($property);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $value);
+    }
+
+    public function resetCustomValidators()
+    {
+        self::setPrivateField(GUMP::class, $this->gump, 'validation_methods', []);
+        self::setPrivateField(GUMP::class, $this->gump, 'validation_methods_errors', []);
+    }
+
+    public function resetCustomFilters()
+    {
+        self::setPrivateField(GUMP::class, $this->gump, 'filter_methods', []);
+    }
+
+    public function resetCustomFieldsLabels()
+    {
+        self::setPrivateField(GUMP::class, $this->gump, 'fields', []);
+    }
+}
---
+++ tests/GetErrorsArray.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests;
+
+use GUMP;
+use Exception;
+
+class GetErrorsArrayTest extends BaseTestCase
+{
+    public function testReturnsEmptyArrayWhenNoErrors()
+    {
+        $result = $this->gump->validate([
+            'test_number' => '111'
+        ], [
+            'test_number' => 'numeric'
+        ]);
+
+        $this->assertEquals([], $this->gump->get_errors_array());
+    }
+
+    public function testReturnsNullWhenNoErrorsAndConvertingToStringIsSet()
+    {
+        $result = $this->gump->validate([
+            'test_number' => '111'
+        ], [
+            'test_number' => 'numeric'
+        ]);
+
+        $this->assertNull($this->gump->get_errors_array(true));
+    }
+
+    public function testReturnsErrorsWithFieldAsKey()
+    {
+        $result = $this->gump->validate([
+            'test_number' => 'aaa'
+        ], [
+            'test_number' => 'numeric'
+        ]);
+
+        $this->assertArrayHasKey('test_number', $this->gump->get_errors_array());
+    }
+
+    public function testPrintsCustomFieldLabelsOnErrors()
+    {
+        GUMP::set_field_name('test', 'Test Num.');
+
+        $result = $this->gump->validate([
+            'test' => 'hey'
+        ], [
+            'test' => 'numeric'
+        ]);
+
+        $this->assertEquals([
+            'test' => 'The Test Num. field must be a number'
+        ], $this->gump->get_errors_array());
+    }
+
+    public function testThrowsExceptionOnValidatorWithoutErrorMessage()
+    {
+        GUMP::add_validator("custom", function($field, $input, $param = NULL) {
+            return $input[$field] === 'ok';
+        });
+
+        $result = $this->gump->validate([
+            'testnumber' => 'hey'
+        ], [
+            'testnumber' => 'custom'
+        ]);
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Rule "custom" does not have an error message');
+
+        $this->gump->get_errors_array();
+    }
+
+
+    public function testEqualsFieldValidator()
+    {
+        GUMP::set_field_name('test_number', 'Test Num.');
+        GUMP::set_field_name('test', 'The Other Test Field');
+
+        $result = $this->gump->validate([
+            'test_number' => '111',
+            'test' => '1112'
+        ], [
+            'test_number' => 'equalsfield,test'
+        ]);
+
+        $this->assertEquals([
+            'test_number' => 'The Test Num. field does not equal The Other Test Field field'
+        ], $this->gump->get_errors_array());
+    }
+}
