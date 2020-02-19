17376b325ee0547c17af6abd2d909d1a0be662ce
Filis Futsarov <filisfutsarov@gmail.com>
2020-02-19 21:15:05 +0100

    Tests updated

---
+++ tests/Validators/ContainsListValidatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Validators;
+
+use GUMP;
+use Exception;
+use Tests\BaseTestCase;
+
+/**
+ * Class ContainsListValidatorTest
+ *
+ * @package Tests
+ */
+class ContainsListValidatorTest extends BaseTestCase
+{
+    public function testSuccess()
+    {
+        $this->assertTrue($this->validate('contains_list,one;two', 'one'));
+    }
+
+    public function testFailure()
+    {
+        $this->assertNotTrue($this->validate('contains_list,one;two', 'three'));
+    }
+}
---
+++ tests/Validators/DoesntContainListValidatorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Validators;
+
+use GUMP;
+use Exception;
+use Tests\BaseTestCase;
+
+/**
+ * Class DoesntContainListValidatorTest
+ *
+ * @package Tests
+ */
+class DoesntContainListValidatorTest extends BaseTestCase
+{
+    public function testSuccess()
+    {
+        $this->assertTrue($this->validate('doesnt_contain_list,one;two', 'three'));
+    }
+
+    public function testFailure()
+    {
+        $this->assertNotTrue($this->validate('doesnt_contain_list,one;two', 'one'));
+    }
+}
