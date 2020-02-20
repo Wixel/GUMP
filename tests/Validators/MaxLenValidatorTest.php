0d3b96e119c8765802bc575f274d3ec4c569f2aa
Filis Futsarov <filisfutsarov@gmail.com>
2020-02-20 23:44:59 +0100

    Tests updated

--- tests/Validators/MaxLenValidatorTest.php
+++ tests/Validators/MaxLenValidatorTest.php
@@ -5,6 +5,7 @@ namespace Tests\Validators;
 use GUMP;
 use Exception;
 use Tests\BaseTestCase;
+use Mockery as m;
 
 /**
  * Class MaxLenValidatorTest
@@ -13,18 +14,76 @@ use Tests\BaseTestCase;
  */
 class MaxLenValidatorTest extends BaseTestCase
 {
-    public function testSuccessWhenEqual()
+    public function testSuccessWhenEqualWithMbStrlen()
     {
-        $this->assertTrue($this->validate('max_len,2', '12'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertTrue($this->validate('max_len,5', 'ñándú'));
     }
 
-    public function testSuccessWhenLess()
+    public function testSuccessWhenLessWithMbStrlen()
     {
-        $this->assertTrue($this->validate('max_len,2', '1'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertTrue($this->validate('max_len,2', 'ñ'));
     }
 
-    public function testErrorWhenMore()
+    public function testErrorWhenMoreWithMbStrlen()
     {
-        $this->assertNotTrue($this->validate('max_len,2', '123'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertNotTrue($this->validate('max_len,2', 'ñán'));
+    }
+
+    public function testSuccessWhenEqualWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertTrue($this->validate('max_len,3', 'ña'));
+        $this->assertTrue($this->validate('max_len,2', 'na'));
+    }
+
+    public function testSuccessWhenLessWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertTrue($this->validate('max_len,2', 'n'));
+    }
+
+    public function testErrorWhenMoreWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertNotTrue($this->validate('max_len,2', 'nan'));
     }
 }
--- tests/Validators/MinLenValidatorTest.php
+++ tests/Validators/MinLenValidatorTest.php
@@ -5,6 +5,7 @@ namespace Tests\Validators;
 use GUMP;
 use Exception;
 use Tests\BaseTestCase;
+use Mockery as m;
 
 /**
  * Class MinLenValidatorTest
@@ -13,18 +14,76 @@ use Tests\BaseTestCase;
  */
 class MinLenValidatorTest extends BaseTestCase
 {
-    public function testSuccessWhenEqual()
+    public function testSuccessWhenEqualWithMbStrlen()
     {
-        $this->assertTrue($this->validate('min_len,2', '12'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertTrue($this->validate('min_len,5', 'ñándú'));
     }
 
-    public function testSuccessWhenMore()
+    public function testSuccessWhenMoreWithMbStrlen()
     {
-        $this->assertTrue($this->validate('min_len,2', '123'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertTrue($this->validate('min_len,2', 'ñán'));
     }
 
-    public function testErrorWhenLess()
+    public function testErrorWhenLessWithMbStrlen()
     {
-        $this->assertNotTrue($this->validate('min_len,2', '1'));
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnTrue();
+
+        $this->assertNotTrue($this->validate('min_len,2', 'ñ'));
+    }
+
+    public function testSuccessWhenEqualWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertTrue($this->validate('min_len,3', 'ña'));
+        $this->assertTrue($this->validate('min_len,2', 'na'));
+    }
+
+    public function testSuccessWhenMoreWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertTrue($this->validate('min_len,2', 'nan'));
+    }
+
+    public function testErrorWhenLessWithStrlen()
+    {
+        $externalMock = m::mock('overload:GUMP\Helpers');
+
+        $externalMock->shouldReceive('functionExists')
+            ->once()
+            ->with('mb_strlen')
+            ->andReturnFalse();
+
+        $this->assertNotTrue($this->validate('min_len,2', 'n'));
     }
 }
