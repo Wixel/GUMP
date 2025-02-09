fd528d64de45f9c3e93d897c679b7c492e7bb77f
Filis Futsarov <filisfutsarov@gmail.com>
2025-02-09 11:51:48 +0100

    update workflow

--- ci/check_validators_translations.php
+++ ci/check_validators_translations.php
@@ -2,20 +2,38 @@
 require_once 'ci/boot.php';
 
 $validators = array_keys(get_gump_validators());
-$translations = array_map(function($item) {
-    return str_replace('validate_', '', $item);
-}, array_keys(require 'lang/en.php'));
 
+$languages = array_filter(scandir('lang'), function ($file) {
+    return !in_array($file, ['.', '..']);
+});
 
-$missingTranslations = array_diff($validators, $translations);
+$totalMissingTranslations = 0;
 
-if (count($missingTranslations) > 0) {
-    foreach ($missingTranslations as $missingTranslation) {
-        print(sprintf('⮕ %s error message is missing!', $missingTranslation).PHP_EOL);
+foreach ($languages as $language) {
+    printf('Checking validators for language %s...'.PHP_EOL.PHP_EOL, $language);
+
+    $translations = array_map(function($item) {
+        return str_replace('validate_', '', $item);
+    }, array_keys(require 'lang/'.$language));
+
+    $missingTranslations = array_diff($validators, $translations);
+
+    $totalMissingTranslations += count($missingTranslations);
+
+    if (count($missingTranslations) > 0) {
+        foreach ($missingTranslations as $missingTranslation) {
+            print(sprintf('⮕ %s error message is missing!', $missingTranslation).PHP_EOL);
+        }
+
+        print(PHP_EOL.'Please add missing translations to lang/en.php file.'.PHP_EOL);
+    } else {
+        print('Translation checks successfully passed!'.PHP_EOL);
     }
 
-    print(PHP_EOL.'Please add missing translations to lang/en.php file.'.PHP_EOL);
+    echo '======================================================'.PHP_EOL.PHP_EOL;
+}
+
+if ($totalMissingTranslations > 0) {
+    printf('%d translations missing'.PHP_EOL, $totalMissingTranslations);
     exit(1);
-} else {
-    print('Translation checks successfully passed!'.PHP_EOL);
 }
